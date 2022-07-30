8db8deadf2bce414d9680eacbb6034a97c96a360
Leandro Almeida <leo.leandro.ars@gmail.com>
2022-07-30 20:06:30 -0300

    Finalização Modal Criar Turma

--- Frontend/Presence/src/Pages/Main/index.js
+++ Frontend/Presence/src/Pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Text, View, Modal, StyleSheet, ProgressViewIOSComponent } from "react-native";
 import Pressables from "../../components/pressables";
+import PressablesModal from "../../components/pressablesModal";
 import { LinearGradient } from 'expo-linear-gradient';
 import { Div } from "./styled";
 import Inputs from "../../components/inputs";
@@ -12,7 +13,7 @@ export default function Main({ navigation }) {
 
 return (
     <Div> 
-        <Pressables iconeM='login' texto='Login' click={() => setModalActive(true)}/>
+        <Pressables iconeFA5='users' texto='Criar uma turma' click={() => setModalActive(true)}/>
 
         <Modal visible={modalActive} animationType='fade' transparent={true} >
             <View style={style.fundoModal}>
@@ -21,12 +22,13 @@ return (
                     colors={['#2C5E7A', '#338995']}
                     start={[ 1.0, 0.5 ]}
                     style={style.modal}
-                    >
-                    <Text style={{fontFamily:'poppinsb', fontSize:15, color:'white'}}>Crie sua turma</Text>
+                >
                     <IconX style={style.close} name='close-circle' size={30} onPress={() => setModalActive(false)}/>
-                    <Inputs place="Matéria" iconeF='book' />
-                    <Inputs place="Turma" iconeO='people'/>
-                    </LinearGradient>
+                    <Text style={{fontFamily:'poppinsb', fontSize:15, color:'white' }}>Crie sua turma</Text>
+                        <Inputs place="Matéria" iconeF='book' />
+                        <Inputs place="Turma" iconeO='people'/>
+                        <PressablesModal texto='Criar' click={() => setModalActive(false)}/>
+                </LinearGradient>
             </View>
         </Modal>
     </Div>
@@ -43,14 +45,15 @@ const style = StyleSheet.create({
     modal:{
         borderRadius: 30,
         padding: 35,
-        width: 350,
-        alignItems: 'center'
+        width: 335,
+        height: 315,
+        alignItems: 'center',
+        justifyContent: 'space-around',
     },
     close:{
         color:"#ffffff",
         position: "absolute",
-        right: 10,
-        top: 10,
-    
+        right: 20,
+        top: 20,
     }
 })
--- Frontend/Presence/src/components/buttons/styled.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components/native";
-
-export const Btn = styled.Button`
-    background-color: black;
-`
