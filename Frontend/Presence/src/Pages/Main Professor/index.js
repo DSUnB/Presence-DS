dde05b86afe71242e7734db752c6febe6ce57a28
Alana Gabriele <alanagabriele43@gmail.com>
2022-08-02 15:32:39 -0300

    arquivos para teste

---
+++ Frontend/Presence/src/Pages/Main Professor/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function Main({ navigation }) {
+
+return (
+    <View>
+        <Text>Você está na Main Professor</Text>
+    </View>
+);
+}
+
+export default Main;
