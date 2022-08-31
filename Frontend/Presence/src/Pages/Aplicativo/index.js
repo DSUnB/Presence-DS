6daa34a6586ddaf3f9b7f400d2738263f5d0c92e
Harleny <101184511+Angelicahaas@users.noreply.github.com>
2022-08-31 11:01:35 -0300

    Mudanças da page Aplicativo
    Co-Authored-By: Leandro Almeida <63979948+LeanArs@users.noreply.github.com>

--- Frontend/Presence/src/Pages/Aplicativo/index.js
+++ Frontend/Presence/src/Pages/Aplicativo/index.js
@@ -6,20 +6,23 @@ import IconI from 'react-native-vector-icons/Ionicons';
 export default function Aplicativo({ navigation }) {
   return (
     <View style={style.container}>
-        <Text style={{fontFamily:'poppinsb', fontSize:20}}>Feudo Verde Pântano</Text>
-        <Image source={logo} style={{width: 100, height: 100}}></Image>
-        <IconI name='code-sharp'></IconI>
-        <Text style={{fontFamily:'poppinsr', fontSize:18}}>Aplicativo Android que tem como objetivo o controle de presença acadêmica, tanto por parte do professor como por parte do aluno.</Text>
-        <IconI name='code-slash'></IconI>
+        <Text style={{fontFamily:'poppinsb', fontSize:20}}>O que é o aplicativo Presence?</Text>
+        <Image source={logo} style={{width: 210, height: 210}}></Image>
+        <View>
+          <IconI name='code-sharp' size={ 20 }></IconI>
+          <Text style={{fontFamily:'poppinsr', fontSize:18, textAlign: 'justify'}}>É um Aplicativo Android que tem como objetivo o controle de presença acadêmica, tanto por parte do professor como por parte do aluno.</Text>
+          <IconI name='code-slash' size={ 20 }></IconI>
+        </View>
     </View>
   )
 }
 
 const style = StyleSheet.create({
     container: {
-        justifyContent: 'center',
+        justifyContent: 'space-evenly',
         alignItems: 'center',
         width: '100%',
         height: '100%',
+        
     }
 })
