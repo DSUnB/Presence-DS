e5cfe648848e98f769441391f7eefcd94f11afcd
Davi Rodrigues da Rocha <davirocha12.80@gmail.com>
2022-09-02 01:38:16 -0300

    correções
    botão de opções estava sendo inutilizado, por esse fator, foi retirado

--- Frontend/Presence/App.js
+++ Frontend/Presence/App.js
@@ -44,7 +44,7 @@ export default function App() {
   <Provider>
     <NavigationContainer>
       <Stack.Navigator 
-        initialRouteName="Login" 
+        initialRouteName="CriarChamada" 
         screenOptions={{
           headerShown: false
         }}
