debf15108adcf174db1ff6901e33360aebf02461
Davi Rodrigues da Rocha <davirocha12.80@gmail.com>
2022-09-02 01:40:06 -0300

    correções

--- Frontend/Presence/App.js
+++ Frontend/Presence/App.js
@@ -44,7 +44,7 @@ export default function App() {
   <Provider>
     <NavigationContainer>
       <Stack.Navigator 
-        initialRouteName="CriarChamada" 
+        initialRouteName="Login" 
         screenOptions={{
           headerShown: false
         }}
